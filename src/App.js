e224786c38eb5d3641d6d33f939632e536ca0227
unknown <gentry_@naver.com>
2022-12-27 15:17:57 +0900

    feat: 좋아요 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import "./css/reset.css";
+import "./css/App.scss";
+import Button from "react-bootstrap/Button";
+import { useState } from "react";
+
 function App() {
-    return <div className="App"></div>;
+    const [likes, setLikes] = useState(0);
+    return (
+        <div className="App">
+            <Button className="likes" variant="outline-primary" onClick={() => setLikes(likes + 1)}>
+                <strong>❤</strong>
+                <span>{likes}</span>
+            </Button>
+        </div>
+    );
 }
 
 export default App;
